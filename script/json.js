eeb41cb04fc74d424c6319d58043a41b51dc88b1
manwize <manwize@live.com>
2015-05-06 13:37:46 -0400

    Auto-Care-Assistant-1.0 ( completed )
    Auto-Care-Assistant-1.0 is designed to help customers keep with vehicle
    warranties and maintenance. Auto-Care-Assistant-1.5 will be created by
    refactoring using AngularJS and other technologies in the MEAN stack.
    Auto-Care-Assistant-2.0 will add a MongoDB database containing
    specifics of warranties, vehicle makes, trims and images.

---
+++ script/json.js
@@ -0,0 +1,50 @@
+ /* 
+	 File       : json.js
+     Author     : Marvin Little 
+     Application: Auto-Care Assitant Application 1.0
+    *************************************************************************************************
+Coming soon:
+     * Auto-Care Assitant Application 1.5 is in progress which will use AngularJS
+     * Auto-Care Assitant Application 2.0 will have a MongoDB database with warranty
+     information, and pictures of the cars.
+
+*/
+
+var json = {
+	"profile1": {
+		"year": ["Vehicle Year:", "2013"],
+		"miles": ["Vehicle Mileage:", "12,221"],
+		"make": ["Vehicle Make:", "Audi"],
+		"model": ["Vehicle Model:", "A8L W12"],
+		"engineSize": ["Engine Size:", "12-cylinder"],
+		"HighPo": ["Engine Booster:", "Twin-Turbocharged"],
+		"date": ["Service Date:", "2013-03-11"],
+		"services": ["Vehicle services:", "Tire rotation"],
+		"warranty": ["Vehicle Warranty:", "150000"],
+		"notes": ["Notes:", "10% off coupon"]
+	},
+	"profile2": {
+		"year": ["Vehicle Year:", "2012"],
+		"miles": ["Vehicle Mileage:", "23,025"],
+		"make": ["Vehicle Make:", "Lincoln"],
+		"model": ["Vehicle Model:", "MKS"],
+		"engineSize": ["Engine Size:", "6-cylinder"],
+		"HighPo": ["Engine Booster:", "Twin-Turbocharged"],
+		"date": ["Service Date:", "2013-03-15"],
+		"services": ["Vehicle services:", "Oil change"],
+		"warranty": ["Vehicle Warranty:", "100000"],
+		"notes": ["Notes:", "Free Tire Rotation on next visit."]
+	},
+	"profile3": {
+		"year": ["Vehicle Year:", "2009"],
+		"miles": ["Vehicle Mileage:", "52,500"],
+		"make": ["Vehicle Make:", "Chevrolet"],
+		"model": ["Vehicle Model:", "Tahoe"],
+		"engineSize": ["Engine Size:", "8-cylinder"],
+		"HighPo": ["Engine Booster:", "Supercharged"],
+		"date": ["Service Date:", "2013-03-25"],
+		"services": ["Vehicle services:", "Tune up"],
+		"warranty": ["Vehicle Warranty:", "120000"],
+		"notes": ["Notes:", "Tire Rotation due at 60,000 miles."]
+	}
+}
